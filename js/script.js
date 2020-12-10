206877e7834e93fc87086bf949be46a407483271
Maksim Timeshin <timeshin0485@gmail.com>
2020-12-11 00:35:48 +0300

    add navbar with adaptive

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
 "use strict";
-
 new WOW().init();
 
-document.addEventListener("DOMContentLoaded", () => { 
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.querySelector("header"),
+        burgerMenu = document.querySelector(".burger__menu"),
+        listHeader = document.querySelector(".list__header");
 
+
+/* ----------- Scroll header ----------- */
+    window.addEventListener("scroll", () => {
+        let pageYOffset = window.pageYOffset;
+        if (pageYOffset <= 0) {
+            header.classList.remove("active__window");
+        } else {
+            header.classList.add("active__window");
+        }
+    });
+    
+   
+
+/* ----------- Burger menu and media nav ----------- */
+
+    burgerMenu.addEventListener("click", () => {
+        burgerMenu.classList.toggle("burger__active");
+    });
+
+    burgerMenu.addEventListener("click", () => {
+        listHeader.classList.toggle("active__header");
+        if (listHeader.classList.contains("active__header")) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "";
+        }
+    });
 });
