b4da8dd44940eb39af7900ee5ad1d0b2e7d21ffc
Maksim Timeshin <timeshin0485@gmail.com>
2021-03-30 03:35:04 +0300

    add 3 section and create send data to fake backend

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
-"use strict";
-
-const wow = new WOW({
-    offset: 0
-});
-wow.init();
+new WOW().init();
 
 document.addEventListener("DOMContentLoaded", () => {
     const header = document.querySelector("header"),
@@ -13,6 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 /* ----------- Scroll header and progress bar ----------- */
+
     window.addEventListener("scroll", () => {
         let pageYOffset = window.pageYOffset;
         if (pageYOffset <= 0) {
@@ -41,11 +37,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     /* Scroll window */
 
-    const scrollbtn = document.querySelector(".skip-slider"),
-        secondSection = document.querySelector(".about__betheme");
+    const scrollbtn = document.querySelector(".skip-slider span"),
+        secondSection = document.querySelector(".about-betheme");
 
     scrollbtn.addEventListener("click", () => {
-        secondSection.scrollIntoView({block: "start",behavior: 'smooth'});
+        secondSection.scrollIntoView({
+            block: "start",
+            behavior: 'smooth'
+        });
     });
    
 
@@ -64,81 +63,241 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    /* ----------- OurLatestWorksPortfolio ----------- */
 
-    /* ----------- Paralax ----------- */
-
-    let scrollPosition = 0,
-    parallaxImg = document.querySelectorAll(".parallax-img");
-    window.addEventListener('scroll', function() {
-        scrollPosition = window.pageYOffset;
-        parallaxImg.forEach(item => {
-            item.style.transform = `translateY(${scrollPosition * 10 / 35}px)`;
-        });
-    });
-
-
-    /* ----------- Slider ----------- */
-
-    const slider = document.querySelector(".slider");
-
-   new Swiper(".slider", {
-        navigation: {
-            nextEl: ".arrow-next",
-            prevEl: '.arrow-prev'
-        },
-        pagination: {
-            el: ".swiper-pagination-fraction",
-
-            type: "fraction",
-            renderFraction: function (currentClass, totalClass) {
-                return '<div class="' + currentClass + '"></div>' +
-                        '<span>/</span>' +
-                        '<div class="' + totalClass + '"></div>';
-            }
-        },
-        on: {
-            transitionEnd: function () {
-                chekSlidesActive();
-            },
-
-            touchStart: function () {
-                slider.style.cursor = "grabbing";
-            },
-
-            touchEnd: function () {
-                slider.style.cursor = "grab";
-            }
-        }
-    });
+    const portfolioItems = document.querySelectorAll(".portfolio-item"),
+        portfolioParent = document.querySelector(".portfolio-list");
     
-    const slidesText = document.querySelectorAll(".slider__text");
 
-    function chekSlidesActive () {
-        slidesText.forEach(slide => {
-            if (slide.parentElement.classList.contains("swiper-slide-active")) {
-                slide.style.display = "flex";
-            } else {
-                slide.style.display = "none";
+    function activeItemElement(i = 0) {
+        portfolioItems[i].classList.add("active__portfolio");
+    }
+
+    activeItemElement();
+
+    function unactiveItemElemen () {
+        portfolioItems.forEach(item => {
+            item.classList.remove("active__portfolio");
+        });
+    }
+
+    // PortfolioFilter
+
+    const portfolioContentParent = document.querySelector(".portfolio-wrap"),
+        portfolioContent = document.querySelectorAll(".portfolio-inner-content");
+        
+    
+    function filterPortfolio (name) {
+        portfolioContentParent.innerHTML = "";
+        portfolioContent.forEach((item) => {
+            if (item.getAttribute("data-filter").split(" ").includes(name)) {
+                portfolioContentParent.append(item);
+            } else if (name == "show all") {
+                portfolioContentParent.append(item);
             }
         });
     }
-    
-    /* Hide elements */
 
-    window.addEventListener("scroll", () => {
-        slidesText.forEach(slide => {
-            if (pageYOffset > 0) {
-                slide.style.opacity = `${(500 - pageYOffset) * 0.002}`;
+    portfolioParent.addEventListener("click", (e) => {
+        portfolioItems.forEach((item, i) => {
+            if (item.classList.contains("active__portfolio")) {
+                
+            } else if (e.target == item){
+                unactiveItemElemen();
+                activeItemElement(i);
+                filterPortfolio(item.textContent);
             }
         });
-        if (pageYOffset > 0) {
-            scrollbtn.style.cssText = `bottom: ${(40 + pageYOffset)}px; opacity: ${(300 - pageYOffset) * 0.00333};`;
-        } else {
-            scrollbtn.style.cssText = `bottom: 40px; opacity: 1;`;
-        }
     });
 
-    /* ----------- SkillBarAnimation ----------- */
 
+    /* ----------- OurTeamSlider ----------- */
+
+
+    new Swiper('.our-team-slider', {
+      cssMode: true,
+      allowTouchMove: false,
+      mousewheel: {
+        invert: true
+      },
+      pagination: {
+        el: '.swiper-bullets',
+        clickable: true,
+      },
+      breakpoints: {
+        0: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+        },
+        480: {
+          slidesPerView: 2,
+        },
+        950: {
+          slidesPerView: 3,
+        },
+        1200: {
+          slidesPerView: 4,
+          slidesPerGroup: 2,
+        }
+      }
+    });
+
+    /* ----------- Slider ----------- */
+
+    new Swiper(".slider", {
+        grabCursor: true,
+         navigation: {
+             nextEl: ".arrow-next",
+             prevEl: '.arrow-prev'
+         },
+         pagination: {
+             el: ".swiper-pagination-fraction",
+ 
+             type: "fraction",
+             renderFraction: function (currentClass, totalClass) {
+                 return '<div class="' + currentClass + '"></div>' +
+                         '<span>/</span>' +
+                         '<div class="' + totalClass + '"></div>';
+             }
+         },
+         on: {
+             transitionEnd: function () {
+                 chekSlidesActive();
+             },
+         }
+     });
+     
+     const slidesText = document.querySelectorAll(".slider__text");
+ 
+     function chekSlidesActive () {
+         slidesText.forEach(slide => {
+             if (slide.parentElement.classList.contains("swiper-slide-active")) {
+                 slide.style.display = "flex";
+             } else {
+                 slide.style.display = "none";
+             }
+         });
+     }
+     
+     /* Hide elements */
+ 
+     window.addEventListener("scroll", () => {
+         slidesText.forEach(slide => {
+             if (window.pageYOffset > 0) {
+                 slide.style.opacity = `${(500 - window.pageYOffset) * 0.002}`;
+             }
+         });
+         if (window.pageYOffset > 0) {
+             scrollbtn.style.cssText = `bottom: ${(40 + window.pageYOffset)}px; opacity: ${(300 - window.pageYOffset) * 0.00333};`;
+         } else {
+             scrollbtn.style.cssText = `bottom: 40px; opacity: 1;`;
+         }
+     });
     
+    /* ----------- Parallax and navigation ----------- */
+
+    const parallaxConteiners = document.querySelectorAll(".parallax"),
+        parallaxImg = document.querySelectorAll(".parallax-img");
+    let scrollPosition = 0;
+
+    function offset(el) {
+        var rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop - document.documentElement.clientHeight, left: rect.left + scrollLeft };
+    }
+
+    function scrollSliderImg () {
+        parallaxImg.forEach(img => {
+            img.style.transform = `translateY(${scrollPosition * 10 / 35}px)`;
+        });
+    }
+
+    function scrollAnotherImg () {
+        parallaxConteiners.forEach(img => {
+            let distanceFromElements = offset(img).top;
+            if (window.pageYOffset >= distanceFromElements) {
+                img.style.cssText = `
+                    background-position-y: ${-500 + (window.pageYOffset - distanceFromElements) * 0.5}px;
+                `;
+            }
+        });
+    }
+
+    const scrollToTheseElements = document.querySelectorAll('.scrollTo'),
+        links = document.querySelectorAll(".list__header .link");
+
+    const linksContainer = [];
+    links.forEach(link => linksContainer.push(link));
+    linksContainer.splice(3, 4);
+    linksContainer.pop();
+
+    function trackScrollPage () {
+        let scrollDistance = window.pageYOffset;
+        
+        scrollToTheseElements.forEach((el, i) => {
+            if (el.offsetTop - document.querySelector('header').clientHeight <= scrollDistance) {
+                linksContainer.forEach((link) => {
+                    if (link.classList.contains("active-li")) {
+                        link.classList.remove("active-li");
+                    }
+                });
+                linksContainer[i].classList.add("active-li");
+            }
+        });
+    }
+    trackScrollPage();
+
+    function scrollToPage() {
+        linksContainer.forEach((link, idx) => {
+            link.addEventListener("click", (e) => {
+                e.preventDefault();
+                let targetSection = scrollToTheseElements[idx];
+                targetSection.scrollIntoView({
+                    block: "start",
+                    behavior: "smooth"
+                });
+            });
+        });
+    }
+    scrollToPage();
+
+
+    window.addEventListener('scroll', function() {
+        scrollSliderImg();
+        scrollAnotherImg();
+        trackScrollPage();
+    });
+
+    /* ----------- Forms(POST DATA) ----------- */
+
+    const forms = document.querySelectorAll("form");
+
+    forms.forEach(item => {
+        bindPostData(item);
+    });
+
+    const postData = async (url, data) => {
+        let res = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: data
+        });
+        return await JSON.parse(res);
+    };
+
+    function bindPostData (form) {
+        form.addEventListener("submit", function (e) {
+            e.preventDefault();
+
+            const formData = new FormData(form);
+
+            const json = JSON.stringify(Object.fromEntries(formData));
+            
+            postData("http://localhost:3000/requests", json);
+        });
+    }
+
 });
